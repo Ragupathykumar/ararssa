9109ca47933ce3f86bc6c82c3195b21811ad7c3c
Ragupathy <kragu892@gmail.com>
2022-08-01 16:51:30 +0530

    arasan

---
+++ src/org/go/ChromeDreiver.java
@@ -0,0 +1,5 @@
+package org.go;
+
+public class ChromeDreiver {
+
+}
---
+++ src/org/go/Done.java
@@ -0,0 +1,16 @@
+package org.go;
+
+import org.openqa.selenium.WebDriver;
+
+public class Done {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", 
+				"C:\\Users\\kragu\\eclipse-workspace\\Don\\driver\\chromedriver.exe");
+		
+		WebDriver sun= new ChromeDreiver();
+		
+		
+	}
+
+}
